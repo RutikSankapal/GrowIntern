13f430ff43fbd2870fa54d223d421f5372406f50
RutikSankapal <145102212+RutikSankapal@users.noreply.github.com>
2023-10-15 21:15:46 +0530

    Add files via upload

---
+++ Task 3 - Calculator/README.md
@@ -0,0 +1,12 @@
+# Calculator App
+
+👉🏻 <a href="https://calculator-app-site.onrender.com/">👀 Live Preview</a>
+
+✓ Built Using HTML5, CSS3 and JavaScript <br />
+✓ Completely Responsive <br />
+✓ Deployed using render
+<br/>
+
+### LICENSE
+
+<a href="./LICENSE">MIT</a>
---
+++ Task 3 - Calculator/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+//////////////////
+// Selec Elements
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["+", "-", "/", "*", "%", "="];
+let output = "";
+
+//////////////////////////////////////
+// Function to calc based on clicked
+const calculate = (btnValue) => {
+  display.focus();
+
+  if (btnValue === "=" && output !== "") {
+    //If output has '%', replace with '/100' before evaluating.
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    //If DEL button is clicked, remove the last character from the output.
+    output = output.toString().slice(0, -1);
+  } else {
+    //If output is empty and button is specialChars then return
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
+
+//////////////////////////////////////////////////
+// Add event listener to btn, call calc() onClick
+buttons.forEach((button) => {
+  // On each btn click - calls function with data-set as argument
+  button.addEventListener("click", (e) => {
+    calculate(e.target.dataset.value);
+  });
+});
